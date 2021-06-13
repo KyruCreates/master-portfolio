ef566e00fad2507ce888b0f9113b081bc46bfd03
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-06-13 02:17:17 -0700

    fixing bootstrap install errors

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 // Import bootstrap
-import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).use(router).mount('#app');
